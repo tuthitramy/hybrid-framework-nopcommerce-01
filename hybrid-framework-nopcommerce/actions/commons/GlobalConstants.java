1f0749d21c593653021531e973c3cc28f3b7bae3
DESKTOP-EGQ2P43\tuthi <tuthitramy290397@gmail.com>
2023-03-15 07:47:57 +0700

    commit global constants

---
+++ hybrid-framework-nopcommerce/actions/commons/GlobalConstants.java
@@ -0,0 +1,10 @@
+package commons;
+
+public class GlobalConstants {
+	public static final String USER_PAGE_URL = "https://demo.nopcommerce.com/";
+	public static final String ADMIN_PAGE_URL = "https://admin-demo.nopcommerce.com/";
+	public static final String PROJECT_PATH = System.getProperty("user.dir");
+	public static final String UPLOAD_FILE = PROJECT_PATH + "\\uploadFiles";
+	public static final long SHORT_TIMEOUT = 5;
+	public static final long LONG_TIMEOUT = 30;
+}
